55e57ec922b5a5211f6afd8b279f66948d8cca96
DuckDiving <bardoyle@icloud.com>
2024-04-24 12:36:19 +0000

    Added warning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var acceptBtn = document.getElementById("acceptBtn");
-    var overlay = document.querySelector(".overlay-jumbotron");
-  
-    acceptBtn.addEventListener("click", function() {
-      console.log("Button clicked"); // Debugging statement
-      if (overlay) {
-        overlay.style.display = "none";
-      }
-    });
+  var acceptBtn = document.getElementById("acceptBtn");
+  var overlay = document.querySelector(".overlay-jumbotron");
+  var popup = document.querySelector(".popup");
+
+  // Function to check if a click event occurred outside of the popup
+  function isOutsidePopup(target) {
+    return !popup.contains(target);
+  }
+
+  acceptBtn.addEventListener("click", function() {
+    overlay.style.display = "none";
   });
+
+  // Add event listener to the overlay
+  overlay.addEventListener("click", function(event) {
+    // Check if the click occurred outside of the popup
+    if (isOutsidePopup(event.target)) {
+      overlay.style.display = "none";
+    }
+  });
+});
